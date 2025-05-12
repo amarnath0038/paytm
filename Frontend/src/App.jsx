775082a67e1a04381a7dc8a0097f123ea6fcab00
Amarnath <amarnathreddy841@gmail.com>
2025-05-12 11:11:34 +0530

    Removed unnecessary comments

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,12 +7,9 @@ import Dashboard from "./pages/Dashboard";
 import Transfer from "./pages/Transfer";
 
 function App() {
+
   return (
-    // <div className="min-h-screen flex items-center justify-center bg-gray-100">
-    //   <div className="text-2xl font-semibold text-gray-800 bg-white px-6 py-4 rounded-xl shadow-md">
-    //     👋 Hi there, welcome to your app!
-    //   </div>
-    // </div>
+
     <BrowserRouter>
       <Routes>
         <Route path = "/signup" element = {<Signup />}></Route>
@@ -21,7 +18,7 @@ function App() {
         <Route path = "/transfer" element = {<Transfer />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
